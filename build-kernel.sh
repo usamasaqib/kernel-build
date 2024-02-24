d36029e3019ea2a9dbd09c47d14e54e88482b238
usamasaqib <usamasaqib.96@live.com>
2024-02-24 14:10:00 +0100

    fix build script

--- build-kernel.sh
+++ build-kernel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euxo pipefail
+set -euo pipefail
 
 DEFAULT_LINUX_SOURCE="./linux-source"
 
@@ -15,6 +15,8 @@ function download_linux_source() {
 }
 
 LINUX_SRC_DIR=""
+GIT_SOURCE=false
+ARCH="x86"
 for i in "$@"; do
     case $i in
         -s=*|--source=*)
@@ -30,7 +32,15 @@ for i in "$@"; do
             shift # past argument=value
             ;;
         --git-source)
-            GIT_SOURCE=YES
+            GIT_SOURCE=true
+            shift # past argument with no value
+            ;;
+        --latest-minor)
+            LATEST_MINOR=true
+            shift # past argument with no value
+            ;;
+        --verbose)
+            set -x
             shift # past argument with no value
             ;;
         -*|--*)
@@ -48,22 +58,51 @@ MINOR=$(echo $TARGET_TAG | cut -d '.' -f 2)
 
 
 if [ "${LINUX_SRC_DIR}" == "" ]; then
+    rm -rf "${DEFAULT_LINUX_SOURCE}"
     download_linux_source $MAJOR $VERSION
 fi
 
-exit 0
+if [ "${GIT_SOURCE}" == "true" ]; then
+    if [ ! -d "${LINUX_SRC_DIR}/.git" ]; then
+        echo "Repository is not a git repository"
+        exit 1
+    fi
+
+    if [ "${LATEST_MINOR}" == "true" ]; then
+        pushd "${LINUX_SRC_DIR}"
+        TARGET_TAG=$(git tag | grep "${TARGET_TAG}" | sort -r -V | head -1)
+        popd
+    fi
+
+    # checkout the specified tag
+    pushd "${LINUX_SRC_DIR}"
+    echo "Checking out tag ${TARGET_TAG}"
+    git checkout "${TARGET_TAG}"
+    popd
+fi
+
+if [ "${ARCH}" == "x86" ]; then
+    BZ_IMAGE=bzImage
+else
+    BZ_IMAGE="Image.gz"
+fi
 
 # Copy extra config to source directory
-cp ./extra.config $LINUX_SRC_DIR/extra.config
+cp ./extra.config "${LINUX_SRC_DIR}"/extra.config
 # Build kernel
-make -C $LINUX_SRC_DIR clean
-make -C $LINUX_SRC_DIR ARCH=$ARCH KCONFIG_CONFIG=start.config defconfig
-tee -a < $LINUX_SRC_DIR/extra.config $LINUX_SRC_DIR/start.config
-make -C $LINUX_SRC_DIR allnoconfig KCONFIG_ALLCONFIG=start.config
-if [[ "$ARCH" == "arm64" && $MAJOR -eq 4 && $MINOR -lt 9 ]]; then echo "kvm_guest.config target not available for $ARCH $TARGET_TAG"; else make -C $LINUX_SRC_DIR kvm_guest.config; fi
-make -j$(nproc) -C $LINUX_SRC_DIR ARCH=$ARCH bindeb-pkg LOCALVERSION=-ddvm
-mkdir kernel-$TARGET_TAG.$ARCH.pkg
-cp $LINUX_SRC_DIR/arch/$ARCH/boot/$BZ_IMAGE kernel-$TARGET_TAG.$ARCH.pkg/bzImage
-cp $LINUX_SRC_DIR/vmlinux kernel-$TARGET_TAG.$ARCH.pkg/vmlinux
-find $LINUX_SRC_DIR/.. -name "linux-headers-$(echo $TARGET_TAG | tr -d 'v')*" -type f | xargs -i cp {} kernel-$TARGET_TAG.$ARCH.pkg/
-find $LINUX_SRC_DIR/.. -name "linux-image-$(echo $TARGET_TAG | tr -d 'v')*" -type f | grep -Fv dbg | xargs -i cp {} kernel-$TARGET_TAG.$ARCH.pkg/
+make -C "${LINUX_SRC_DIR}" clean
+make -C "${LINUX_SRC_DIR}" ARCH="${ARCH}" KCONFIG_CONFIG=start.config defconfig
+tee -a < "${LINUX_SRC_DIR}"/extra.config "${LINUX_SRC_DIR}"/start.config
+make -C "${LINUX_SRC_DIR}" allnoconfig KCONFIG_ALLCONFIG=start.config
+if [[ "${ARCH}" == "arm64" && "${MAJOR}" -eq 4 && "${MINOR}" -lt 9 ]]; then echo "kvm_guest.config target not available for ${ARCH} ${TARGET_TAG}"; else make -C "${LINUX_SRC_DIR}" kvm_guest.config; fi
+make -j$(nproc) -C "${LINUX_SRC_DIR}" ARCH="${ARCH}" bindeb-pkg LOCALVERSION=-ddvm
+
+package=kernel-"${TARGET_TAG}"."${ARCH}".pkg
+if [ -d "${package}" ]; then
+    rm -rf "${package}"
+fi
+
+cp "${LINUX_SRC_DIR}"/arch/"${ARCH}"/boot/"${BZ_IMAGE}" "${package}"
+cp "${LINUX_SRC_DIR}"/vmlinux "${package}"
+find "${LINUX_SRC_DIR}"/.. -name "linux-headers-$(echo "${TARGET_TAG}" | tr -d 'v')*" -type f | xargs -i cp {} "${package}"
+find "${LINUX_SRC_DIR}"/.. -name "linux-image-$(echo $TARGET_TAG | tr -d 'v')*" -type f | grep -Fv dbg | xargs -i cp {} "${package}"
