60a4ff45dd486aba8daf64f9328e24d37f67a1bf
usamasaqib <usamasaqib.96@live.com>
2024-05-27 19:44:14 +0200

    install pahole

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -11,4 +11,16 @@ sudo apt install -y git \
     libelf-dev \
     cpio \
     build-essential \
-    libssl-dev
+    libssl-dev \
+    debhelper-compat \
+    debootstrap \
+    cmake
+
+git -c http.sslVerify=false clone --recurse-submodules https://github.com/acmel/dwarves.git /tmp/dwarves
+cd dwarves
+git config http.sslVerify "false"
+git checkout v1.22
+mkdir build
+cd build
+cmake -D__LIB=lib -DCMAKE_INSTALL_PREFIX=/usr/ ..
+make install
