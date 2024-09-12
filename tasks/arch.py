a9d304aecdafeff513580cfd823b00b05690e7d1
usamasaqib <usamasaqib.96@live.com>
2024-09-12 13:49:14 +0200

    remove comments

--- tasks/arch.py
+++ tasks/arch.py
@@ -1,7 +1,3 @@
-"""
-Standardized management of architecture values
-"""
-
 from __future__ import annotations
 
 import platform
@@ -11,18 +7,6 @@ KbuildArchName = Literal['x86', 'arm64']
 
 
 class Arch:
-    """A class representing an architecture. Contains all the names that this arch
-    might have depending on where it is used.
-
-    For example, the AMD64 architecture is known as "amd64" in Go, "x86_64" in GCC,
-    "x64" in Windows, etc. With this class we just pass around a single object, enabling
-    easy comparisons (Arch objects implement __eq__ and __hash__) and easy access to the
-    different names.
-
-    Use Arch.from_str to convert from a string to an Arch object.
-    Use Arch.local() to get the Arch object for the current machine's architecture.
-    """
-
     def __init__(
         self,
         name: str,
@@ -31,21 +15,14 @@ class Arch:
         kbuild_arch: KbuildArchName | None,
         spellings: set[str],
     ):
-        self.name = name  #: Unique name for this architecture within this file.
-        self.spellings = spellings  #: All the possible names for this architecture. Will be used when parsing user input.
-        self.gcc_arch = gcc_arch  #: Architecture used for GCC
-        self._kbuild_arch: KbuildArchName | None = (
-            kbuild_arch  #: Architecture used for build, if supported
-        )
-        self.kernel_arch = kernel_arch  #: Name for the architecture in the Linux kernel
+        self.name = name
+        self.spellings = spellings
+        self.gcc_arch = gcc_arch
+        self._kbuild_arch: KbuildArchName | None = kbuild_arch
+        self.kernel_arch = kernel_arch
 
     @property
     def kbuild_arch(self) -> KbuildArchName:
-        """
-        Return the kernel build arch name for this architecture. Raises ValueError if not defined.
-
-        Useful to avoid constant None checks
-        """
         if self._kbuild_arch is None:
             raise ValueError(f"Kernel build arch not defined for {self.name}")
         return self._kbuild_arch
@@ -66,21 +43,12 @@ class Arch:
 
     @staticmethod
     def from_str(arch: str | Literal["local"] | Arch) -> Arch:
-        """Parse a string into an Arch object. If the input is already an Arch object, it is returned as is.
-
-        If the input is "local", the current machine's architecture is used, can be used as a shorthand
-        instead of Arch.from_str(platform.machine()).
-
-        Will raise KeyError if the architecture is not recognized.
-        """
         if isinstance(arch, Arch):
             return arch
 
         if arch == "local":
             arch = platform.machine().lower()
 
-        # Not the most efficient way to do this, but the list is small
-        # enough and this way we avoid having to maintain a dictionary
         for arch_obj in ALL_ARCHS:
             if arch.lower() in arch_obj.spellings:
                 return arch_obj
@@ -88,7 +56,6 @@ class Arch:
 
     @staticmethod
     def local() -> Arch:
-        """Shorthand to return the Arch object for the current machine's architecture."""
         return Arch.from_str("local")
 
 
